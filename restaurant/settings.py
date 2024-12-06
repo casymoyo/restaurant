e2713b791b7c5020e38e395be340977abcacf948
Casper Moyo <cassymyo@gmai.com>
2024-12-06 09:58:55 +0000

    merge

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -105,9 +105,14 @@ LOGIN_URL = "users:login"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': dj_database_url.config(
-    #     default='postgresql://postgres:bsgtOvAIFbBQgIcHLvlUFlvIiAiapbHm@autorack.proxy.rlwy.net:44818/railway'
-    # )
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',  
+        'USER': 'postgres',
+        'PASSWORD': 'neverfail',
+        'HOST': '192.168.10.173',
+        'PORT': '5433',
+    }
     # 'default': {
     #     'ENGINE': 'django.db.backends.postgresql',
     #     'NAME': 'urban_eats',  
@@ -116,14 +121,6 @@ DATABASES = {
     #     # 'HOST': '192.168.10.173',
     #     'PORT': '5432',
     # }
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'urban_eats',  
-        'USER': 'postgres',
-        'PASSWORD': 'neverfail',
-        'HOST': '192.168.10.173',
-        'PORT': '5433',
-    }
 }
 
 AUTH_USER_MODEL = 'users.User'
