caef9d8d31023d6c87f7fd54c74cd9e7503a9937
casy <cassymyo@gmail.com>
2025-02-13 01:14:34 +0200

    comit

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -104,25 +104,9 @@ LOGIN_URL = "users:login"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': dj_database_url.config(
-    #     default='postgresql://postgres:bsgtOvAIFbBQgIcHLvlUFlvIiAiapbHm@autorack.proxy.rlwy.net:44818/railway'
-    # )
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'trial',  
-        'USER': 'postgres',
-        'PASSWORD': 'neverfail',
-        # 'HOST': '192.168.10.173',
-        'PORT': '5432',
-    }
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': 'postgres',  
-    #     'USER': 'postgres',
-    #     'PASSWORD': 'neverfail',
-    #     'HOST': '192.168.10.173',
-    #     'PORT': '5433',
-    # }
+    'default': dj_database_url.config(
+        default='postgresql://postgres:bsgtOvAIFbBQgIcHLvlUFlvIiAiapbHm@autorack.proxy.rlwy.net:44818/railway'
+    )
 }
 
 AUTH_USER_MODEL = 'users.User'
